28f8291de6446ae72006bd2ab8de8e79e4ccceee
Walter <walter.bg7@gmail.com>
2019-11-05 23:13:14 -0600

    Correcciones, Comentarios y Avances en el SW

--- hiloPrincipal.py
+++ hiloPrincipal.py
@@ -15,15 +15,16 @@ class HiloPrincipal():
         self.candadoBusInst = threading.Lock() # Candado para aceso a la memoria de Instrucciones
         self.candadoBusDatos = threading.Lock() # Candado para aceso a la memoria de datos
         self.candadoTCB = threading.Lock() # Candado para acceso a la TCB
-        self.candadoCacheNucleo0 = threading.Lock()
-        self.candadoCacheNucleo1 = threading.Lock()
+        self.candadoCacheNucleo0 = threading.Lock() # Candado para la cache de datos del nucleo 0
+        self.candadoCacheNucleo1 = threading.Lock() # Candado para la cache de datos del nucleo 1
+        self.candadoEscritura = threading.Lock() # Candado para escribir en consola
         self.barrera = threading.Barrier(2) # Barrera para controlar sincronizacion en ciclos de reloj
-        
+        # Cache de Datos del nucleo 0
         self.cacheDatosN0 = [[0,0,0,0,-1,0], 
                              [0,0,0,0,-1,0],
                              [0,0,0,0,-1,0],
                              [0,0,0,0,-1,0]]
-        
+        # Cache de Datos del nucleo 1
         self.cacheDatosN1 = [[0,0,0,0,-1,0], 
                              [0,0,0,0,-1,0],
                              [0,0,0,0,-1,0],
@@ -39,7 +40,7 @@ class HiloPrincipal():
         nucleo0 = hiloDeNucleo.HiloDeNucleo(0, self.tcb, self.memInst, self.memDatos, self.candadoBusInst, 
                                             self.candadoBusDatos, self.candadoTCB, self.barrera,
                                             self.candadoCacheNucleo0, self.candadoCacheNucleo1, 
-                                            self.cacheDatosN0, self.cacheDatosN1)
+                                            self.cacheDatosN0, self.cacheDatosN1, self.candadoEscritura)
         # Se crea el hilo
         nucleo0Thread = threading.Thread(target=nucleo0.run, args=())
         # Se corre el hilo
@@ -48,18 +49,19 @@ class HiloPrincipal():
         nucleo1 = hiloDeNucleo.HiloDeNucleo(1, self.tcb, self.memInst, self.memDatos, self.candadoBusInst, 
                                             self.candadoBusDatos, self.candadoTCB, self.barrera, 
                                             self.candadoCacheNucleo1, self.candadoCacheNucleo0,
-                                            self.cacheDatosN1, self.cacheDatosN0)
+                                            self.cacheDatosN1, self.cacheDatosN0, self.candadoEscritura)
         # Se crea el hilo
         nucleo1Thread = threading.Thread(target=nucleo1.run, args=())
         # Se corre el hilo
         nucleo1Thread.start()
-        
+        self.candadoEscritura.acquire()
         print("Si bueno, quien tiene hambre \n")
+        self.candadoEscritura.release()
         
     def llenarMenDatos(self):
         for i in range(0,96):
             self.memDatos.append(i*4)
-        print(self.memDatos)
+        print("Mem Datos: \n", self.memDatos)
             
     # Se crea una lista de diccionarios, donde cada indice de la lista es 
     def llenarTCB(self, pc, nombre_archivo):
@@ -71,13 +73,11 @@ class HiloPrincipal():
         self.tcb.append(diccionario)
     
     def llenarMenInst(self):
-        print("Llenando... jaja")
         indiceMemInst = 383
         for arch in range(0,7):
             nombre_archivo = str(arch) + ".txt"
             f = open(nombre_archivo, 'r')
             contenido = f.read()
-            print(contenido)
             instrucciones = contenido.split("\n")
             #contadorInst = 1
             ponerEnTCB = False
@@ -93,5 +93,5 @@ class HiloPrincipal():
                     except ValueError:
                         pass
             f.close()
-        print(self.memInst)
-        print(self.tcb)
+        print("Mem Instucciones: \n", self.memInst)
+        print("TCB: \n", self.tcb)
